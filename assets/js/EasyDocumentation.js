854d3c70bea5cf467a024030b6910f0160d908a7
Quinten De Swaef <quintendeswaef@gmail.com>
2014-09-18 15:30:47 +0200

    remove logging from easydocumentation

--- assets/js/EasyDocumentation.js
+++ assets/js/EasyDocumentation.js
@@ -3,11 +3,8 @@ var EasyDocumentation = {}
 EasyDocumentation.initializeMarkdown = function () {
     $(".documentation").each(function () {
         var target = $(this);
-        console.log($(this));
         $.get("assets/md/" + $(this).data("md-file"), function (data) {
-            console.log(data);
             var converter = new Markdown.Converter();
-            console.log($(this));
             target.html(converter.makeHtml(data));
         });
     });
